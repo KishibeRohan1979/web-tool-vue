04376b21b0c31d2a1a5a7e20c01454e9eb746c50
康旭东 <kangxudong@piesat.com>
2023-05-26 18:33:04 +0800

    🎉添加头像显示，头像框显示，昵称显示

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -227,7 +227,217 @@
               <!-- 评论区 -->
               <div v-for="comment in comments" class="reply-item" :key="comment.rpid"
                    :style="getCommentStyles(comment)">
-                6
+                <!-- 头像+评论整合 -->
+                <div class="root-reply-container" data-v-7592db79="">
+                  <!-- 头像区 -->
+                  <div class="root-reply-avatar" :data-user-id="comment.userid"
+                       :data-root-reply-id="comment.rpid" data-v-7592db79="">
+                    <!-- 头像 -->
+                    <div class="avatar" data-v-7592db79="">
+                      <!-- 头像 -->
+                      <div class="bili-avatar" :style="biliAvatarStyle(comment)">
+                        <img class="bili-avatar-img bili-avatar-face bili-avatar-img-radius"
+                             :data-src= "comment.avatar + '@160w_160h_1c_1s_!web-avatar-comment.webp'"
+                             alt=""
+                             :src="comment.avatar + '@160w_160h_1c_1s_!web-avatar-comment.webp'">
+                        <!-- 头像框 -->
+                        <div class="bili-avatar-pendent-dom" v-if="getUserSailingPendantImageEnhance(comment)">
+                          <img class="bili-avatar-img"
+                               :data-src="comment.userSailing.pendant.image_enhance+'@240w_240h_!web-avatar-comment.webp'"
+                               alt=""
+                               :src="comment.userSailing.pendant.image_enhance+'@240w_240h_!web-avatar-comment.webp'">
+                        </div>
+                        <!-- 闪电标志 -->
+<!--                        <span class="bili-avatar-icon bili-avatar-right-icon  bili-avatar-icon-personal bili-avatar-size-48"></span>-->
+                      </div>
+                    </div>
+                  </div>
+                  <!-- 整个评论内容部分 -->
+                  <div class="content-warp" data-v-7592db79="">
+                    <!-- 右上角装饰部分、类似我的崩坏三雷鸣那个东西 -->
+                    <div class="reply-decorate" data-v-7592db79="">
+                      <!-- 显示装饰 -->
+                      <div class="user-sailing" data-v-7592db79="">
+                        <img class="user-sailing-img"
+                             :src="getUserSailingCardbgImage(comment)"
+                             data-v-7592db79="">
+                        <!-- 装饰的编号 -->
+                        <div class="user-sailing-text" data-v-7592db79="" v-if="isFan(comment)">
+                          <span class=" sailing-text"
+                                data-v-7592db79="">
+                              NO.
+                          </span>
+                          <br data-v-7592db79="">
+                          <span class="sailing-text" data-v-7592db79="">
+                              {{comment.userSailing.cardbg.fan.num_desc}}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- 用户名字及其渲染 -->
+                    <div class="user-info" data-v-7592db79="">
+                      <!-- 用户名字 -->
+                      <div class="user-name" :data-user-id="comment.userid"
+                           :data-root-reply-id="comment.rpid" data-v-7592db79="">
+                        {{comment.uname}}
+                      </div>
+                      <!-- 用户等级 -->
+                      <i :class="userLevel(comment)" data-v-7592db79=""
+                         style="width: 30px; height: 30px;">
+                        <svg version="1.1" id="图层_1"
+                             xmlns="http://www.w3.org/2000/svg"
+                             xmlns:xlink="http://www.w3.org/1999/xlink"
+                             x="0px" y="0px" viewBox="0 0 30 30"
+                             style="enable-background:new 0 0 30 30;"
+                             xml:space="preserve">
+                                  <component is="style" type="text/css">
+                                      .st0.lv6 {
+                                        fill: transparent;
+                                      }
+
+                                      .st1.lv6 {
+                                        fill-rule: evenodd;
+                                        clip-rule: evenodd;
+                                        fill: #FFFFFF;
+                                      }
+
+                                      .st2.lv6 {
+                                        fill-rule: evenodd;
+                                        clip-rule: evenodd;
+                                        fill: #F04C49;
+                                      }
+                                  </component>
+                          <rect class="st0 lv6" width="30" height="30"></rect>
+                          <path class="st1 lv6"
+                                d="M19.9,11.1h-5.6v1.1H2.1v6.7h17.8v-6.7h0V11.1z">
+                            </path>
+                          <path class="st2 lv6"
+                                d="M14,10c-0.4,0-0.6,0.3-0.6,0.6V11H1.6C1.2,11,1,11.3,1,11.6v7.8C1,19.8,1.3,20,1.6,20h18.8 c0.4,0,0.6-0.3,0.6-0.6v-8.8c0.1-0.3-0.2-0.6-0.5-0.6H14z M19.8,12.3c0,0.2-0.2,0.4-0.4,0.4H16v1.6h3.4c0.2,0,0.4,0.2,0.4,0.4v3.7 c0,0.2-0.2,0.4-0.4,0.4H15c-0.2,0-0.4-0.2-0.4-0.4v-6.8c0-0.2,0.2-0.4,0.4-0.4h4.4c0.2,0,0.4,0.2,0.4,0.4V12.3z M12.6,16.7 c0.1-0.1,0.1-0.2,0.1-0.4v-0.1v-3.4c0-0.2-0.2-0.4-0.4-0.4h-0.6c-0.2,0-0.4,0.2-0.4,0.4v3.3L10,17.3l-1.2-1.2v-3.4 c0-0.2-0.2-0.4-0.4-0.4H7.8c-0.2,0-0.4,0.2-0.4,0.4v3.5v0.1c0,0.2,0,0.4,0.1,0.4l2,2c0.1,0.1,0.5,0.1,0.5,0.1s0.4,0,0.5-0.1 L12.6,16.7z M6.1,18.9H2.6c-0.2,0-0.4-0.2-0.4-0.4v-5.8c0-0.2,0.2-0.4,0.4-0.4h0.6c0.2,0,0.4,0.2,0.4,0.4v4.7h2.4 c0.2,0,0.4,0.2,0.4,0.4v0.6C6.5,18.7,6.3,18.9,6.1,18.9z M18.4,17.4H16v-1.7h2.4V17.4z">
+                            </path>
+                        </svg>
+                      </i>
+
+                      <!-- up主标记 -->
+                      <i class="svg-icon up-web up-icon" data-v-7592db79=""
+                         style="width: 24px; height: 24px;">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                          <rect x="3" y="6" width="18" height="11.5" rx="2"
+                                fill="#FF6699"></rect>
+                          <path
+                              d="M5.7 8.36V12.79C5.7 13.72 5.96 14.43 6.49 14.93C6.99 15.4 7.72 15.64 8.67 15.64C9.61 15.64 10.34 15.4 10.86 14.92C11.38 14.43 11.64 13.72 11.64 12.79V8.36H10.47V12.81C10.47 13.43 10.32 13.88 10.04 14.18C9.75 14.47 9.29 14.62 8.67 14.62C8.04 14.62 7.58 14.47 7.3 14.18C7.01 13.88 6.87 13.43 6.87 12.81V8.36H5.7ZM13.0438 8.36V15.5H14.2138V12.76H15.9838C17.7238 12.76 18.5938 12.02 18.5938 10.55C18.5938 9.09 17.7238 8.36 16.0038 8.36H13.0438ZM14.2138 9.36H15.9138C16.4238 9.36 16.8038 9.45 17.0438 9.64C17.2838 9.82 17.4138 10.12 17.4138 10.55C17.4138 10.98 17.2938 11.29 17.0538 11.48C16.8138 11.66 16.4338 11.76 15.9138 11.76H14.2138V9.36Z"
+                              fill="white">
+                          </path>
+                        </svg>
+                      </i>
+
+                      <!-- 粉丝牌子：粉丝牌子名称、等级 -->
+                      <div class="fan-badge" data-v-7592db79="">
+                        <!-- 粉丝牌子名称 -->
+                        <div class="badge-name-wrap" data-v-7592db79="">
+                          <div class="badge-name" data-v-7592db79="">男女男</div>
+                        </div>
+                        <!-- 粉丝牌子等级 -->
+                        <div class="badge-level-wrap" data-v-7592db79="">
+                          <div class="badge-level" data-v-7592db79="">12</div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- 评论、时间、点赞、图片等 -->
+                    <div class="root-reply" data-v-7592db79="">
+                      <!-- 评论的内容论点，这里的span不要换行重排代码格式 -->
+                      <span class="reply-content-container root-reply" data-v-7592db79="">
+                            <span class="reply-content"><i class="top-icon">置顶</i>BGM：<a
+                                class="jump-link search-word"
+                                data-url="//search.bilibili.com/all?from_source=webcommentline_search&amp;keyword=%E4%B9%B1%E4%B8%96%E5%B7%A8%E6%98%9F"
+                                data-search-key="乱世巨星">乱世巨星</a><i
+                                class="icon search-word"
+                                style="background-image: url(https://i0.hdslb.com/bfs/reply/9f3ad0659e84c96a711b88dd33f4bc2e945045e0.png)"></i><br>希望大家把正能量传播下去<img
+                                class="emoji-small"
+                                src="//i0.hdslb.com/bfs/emote/b5a5898491944a4268360f2e7a84623149672eb6.png@48w_48h.webp"
+                                alt="[呲牙]"><br>喜欢视频的观众朋友记得点赞硬币支持一下<img
+                                class="emoji-small"
+                                src="//i0.hdslb.com/bfs/emote/b5a5898491944a4268360f2e7a84623149672eb6.png@48w_48h.webp"
+                                alt="[呲牙]"><br>不要剧透<img
+                                class="emoji-small"
+                                src="//i0.hdslb.com/bfs/emote/cb0ebbd0668640f07ebfc0e03f7a18a8cd00b4ed.png@48w_48h.webp"
+                                alt="[冷]">不要去不相关的视频刷我这样很不好<img
+                                class="emoji-small"
+                                src="//i0.hdslb.com/bfs/emote/cb0ebbd0668640f07ebfc0e03f7a18a8cd00b4ed.png@48w_48h.webp"
+                                alt="[冷]"><br>再次谢谢各位的支持<img
+                                class="emoji-small"
+                                src="//i0.hdslb.com/bfs/emote/431432c43da3ee5aab5b0e4f8931953e649e9975.png@48w_48h.webp"
+                                alt="[打call]"></span>
+                        </span>
+                      <!-- 评论的图片 -->
+                      <div class="image-exhibition" data-v-67b4dc9c="" data-v-7592db79="">
+                        <!-- 评论楼层预览区 -->
+                        <div class="preview-image-container" data-v-67b4dc9c="">
+                          <!-- 一个div是一个图片 -->
+                          <div class="image-item-wrap vertical" data-v-67b4dc9c=""
+                               style="width: 120px; height: 120px;">
+                            <img
+                                src="https://i0.hdslb.com/bfs/new_dyn/270ccbb1a49c846a7c3d92ec14ca17c14937182.png@120w_120h_1c_!web-comment-note.webp"
+                                data-v-67b4dc9c="">
+                          </div>
+                          <div class="image-item-wrap vertical" data-v-67b4dc9c=""
+                               style="width: 120px; height: 120px;">
+                            <img
+                                src="https://i0.hdslb.com/bfs/new_dyn/6896691e6bd5c9e5bc2d49e1e2b9223d4937182.png@120w_120h_1c_!web-comment-note.webp"
+                                data-v-67b4dc9c="">
+                          </div>
+                        </div>
+                      </div>
+                      <!-- 时间、点赞、拉踩、回复按钮 -->
+                      <div class="reply-info" data-v-7592db79="">
+                        <!-- 时间 -->
+                        <span class="reply-time"
+                              data-v-7592db79="">2023-04-04 18:33</span>
+                        <!-- 点赞 -->
+                        <span class="reply-like" data-v-7592db79="">
+                            <i class="svg-icon like use-color like-icon"
+                               data-v-7592db79=""
+                               style="width: 16px; height: 16px;">
+                                <svg t="1636093575017" class="icon"
+                                     viewBox="0 0 1024 1024"
+                                     version="1.1"
+                                     xmlns="http://www.w3.org/2000/svg"
+                                     p-id="3323"
+                                     width="200" height="200">
+                                    <path
+                                        d="M594.176 151.168a34.048 34.048 0 0 0-29.184 10.816c-11.264 13.184-15.872 24.064-21.504 40.064l-1.92 5.632c-5.632 16.128-12.8 36.864-27.648 63.232-25.408 44.928-50.304 74.432-86.208 97.024-23.04 14.528-43.648 26.368-65.024 32.576v419.648a4569.408 4569.408 0 0 0 339.072-4.672c38.72-2.048 72-21.12 88.96-52.032 21.504-39.36 47.168-95.744 63.552-163.008a782.72 782.72 0 0 0 22.528-163.008c0.448-16.832-13.44-32.256-35.328-32.256h-197.312a32 32 0 0 1-28.608-46.336l0.192-0.32 0.64-1.344 2.56-5.504c2.112-4.8 5.12-11.776 8.32-20.16 6.592-17.088 13.568-39.04 16.768-60.416 4.992-33.344 3.776-60.16-9.344-84.992-14.08-26.688-30.016-33.728-40.512-34.944zM691.84 341.12h149.568c52.736 0 100.864 40.192 99.328 98.048a845.888 845.888 0 0 1-24.32 176.384 742.336 742.336 0 0 1-69.632 178.56c-29.184 53.44-84.48 82.304-141.76 85.248-55.68 2.88-138.304 5.952-235.712 5.952-96 0-183.552-3.008-244.672-5.76-66.432-3.136-123.392-51.392-131.008-119.872a1380.672 1380.672 0 0 1-0.768-296.704c7.68-72.768 70.4-121.792 140.032-121.792h97.728c13.76 0 28.16-5.504 62.976-27.456 24.064-15.104 42.432-35.2 64.512-74.24 11.904-21.184 17.408-36.928 22.912-52.8l2.048-5.888c6.656-18.88 14.4-38.4 33.28-60.416a97.984 97.984 0 0 1 85.12-32.768c35.264 4.096 67.776 26.88 89.792 68.608 22.208 42.176 21.888 84.864 16 124.352a342.464 342.464 0 0 1-15.424 60.544z m-393.216 477.248V405.184H232.96c-40.448 0-72.448 27.712-76.352 64.512a1318.912 1318.912 0 0 0 0.64 282.88c3.904 34.752 32.96 61.248 70.4 62.976 20.8 0.96 44.8 1.92 71.04 2.816z"
+                                        p-id="3324" fill="#9499a0">
+                                    </path>
+                                </svg>
+                            </i>
+                          <!-- 点赞数 -->
+                            <span data-v-7592db79="">2782</span>
+                        </span>
+                        <!-- 拉踩按钮 -->
+                        <span class="reply-dislike" data-v-7592db79="">
+                            <i class="svg-icon dislike use-color dislike-icon"
+                               data-v-7592db79=""
+                               style="width: 16px; height: 16px;">
+                                <svg t="1636093677814" class="icon"
+                                     viewBox="0 0 1024 1024"
+                                     version="1.1"
+                                     xmlns="http://www.w3.org/2000/svg"
+                                     p-id="3933" width="200"
+                                     height="200">
+                                    <path
+                                        d="M594.112 872.768a34.048 34.048 0 0 1-29.12-10.816c-11.264-13.248-15.872-24.064-21.504-40.064l-1.92-5.632c-5.632-16.128-12.8-36.864-27.712-63.232-25.344-44.928-50.24-74.432-86.144-97.024-23.104-14.528-43.648-26.432-65.024-32.64V203.84a4570.24 4570.24 0 0 1 339.072 4.672c38.656 2.048 72 21.12 88.896 52.032 21.504 39.36 47.232 95.744 63.552 163.008 16.448 67.52 21.568 123.776 22.592 163.008 0.448 16.832-13.44 32.256-35.392 32.256h-197.248a32 32 0 0 0-28.608 46.336l0.128 0.32 0.64 1.28 2.56 5.568c2.176 4.8 5.12 11.776 8.384 20.16 6.528 17.088 13.568 39.04 16.768 60.416 4.928 33.344 3.712 60.16-9.344 84.992-14.08 26.688-30.016 33.728-40.576 34.944z m97.728-190.016h149.568c52.8 0 100.864-40.128 99.392-97.92a846.336 846.336 0 0 0-24.32-176.448 742.016 742.016 0 0 0-69.632-178.56c-29.248-53.44-84.48-82.304-141.824-85.248-55.68-2.88-138.24-5.952-235.712-5.952-96 0-183.488 3.008-244.672 5.76-66.368 3.136-123.328 51.392-130.944 119.872a1380.608 1380.608 0 0 0-0.768 296.704c7.68 72.768 70.4 121.792 140.032 121.792h97.728c13.76 0 28.16 5.504 62.976 27.392 24.064 15.168 42.432 35.264 64.448 74.368 11.968 21.12 17.472 36.864 22.976 52.736l2.048 5.888c6.656 18.88 14.336 38.4 33.216 60.416 19.456 22.72 51.456 36.736 85.184 32.768 35.2-4.096 67.776-26.88 89.792-68.672 22.208-42.112 21.888-84.8 16-124.288a343.04 343.04 0 0 0-15.488-60.608zM298.688 205.568v413.184H232.96c-40.512 0-72.448-27.712-76.352-64.512a1318.912 1318.912 0 0 1 0.64-282.88c3.904-34.816 32.896-61.248 70.4-62.976 20.8-0.96 44.736-1.92 71.04-2.816z"
+                                        p-id="3934" fill="#9499a0">
+                                    </path>
+                                </svg>
+                            </i>
+                        </span>
+                        <!-- 回复按钮 -->
+                        <span class="reply-btn" data-v-7592db79="">共x条回复</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="bottom-line" data-v-7592db79=""></div>
               </div>
 
             </div>
@@ -338,6 +548,64 @@ export default {
         styles["--1996face"] = cardbgColor;
       }
       return styles;
+    },
+    // 获取头像框
+    getUserSailingPendantImageEnhance(comment) {
+      if (Object.keys(comment.userSailing).length !== 0 && (comment.userSailing).hasOwnProperty("pendant")) {
+        return comment.userSailing.pendant.image_enhance;
+      } else {
+        return "";
+      }
+    },
+    // 动态修改头像大小
+    biliAvatarStyle(comment) {
+      if (Object.keys(comment.userSailing).length !== 0 && (comment.userSailing).hasOwnProperty("pendant")) {
+        return {
+          "width": "34px",
+          "height":"34px"
+        };
+      } else {
+        return {
+          "width": "40px",
+          "height":"40px"
+        };
+      }
+    },
+    // 右上角装扮的url地址
+    getUserSailingCardbgImage(comment) {
+      if (Object.keys(comment.userSailing).length !== 0 && (comment.userSailing).hasOwnProperty("cardbg")) {
+        return comment.userSailing.cardbg.image + "@576w.webp";
+      } else {
+        return "";
+      }
+    },
+    // 判断是不是装扮牌子的粉丝
+    isFan(comment) {
+      if (Object.keys(comment.userSailing).length !== 0 && (comment.userSailing).hasOwnProperty("cardbg")) {
+        return comment.userSailing.cardbg.fan.is_fan === 1;
+      } else {
+        return false;
+      }
+    },
+    // 改变userLevel的class
+    userLevel(comment) {
+      const level = comment.userLevel;
+      switch (level) {
+        case 0:
+          return "";
+        case 1:
+          return "";
+        case 2:
+          return "";
+        case 3:
+          return "";
+        case 4:
+          return "";
+        case 5:
+          return "";
+        case 6:
+          return "";
+      }
     }
   }
 }
