e0f88b86517a4a7e02fe4981441989ee3323632a
康旭东 <kangxudong@piesat.cn>
2023-05-28 04:10:46 +0800

    ⚡项目基本完成完成，改了一下文件地址

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+// import {createApp} from 'vue'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
 import {createApp} from 'vue'
-import App from './App.vue'
+import MyApp from 'components/MyApp.vue'
 
-createApp(App).mount('#app')
+const myapp = createApp(MyApp);
+myapp.mount('#myapp')
